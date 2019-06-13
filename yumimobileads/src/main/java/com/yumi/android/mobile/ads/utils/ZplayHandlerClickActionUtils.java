a455e1e40e8da649c6f62a5b2d4eb57d532f6e1c
“yangfengbang” <yangfengbang@zplay.cn>
2019-06-13 15:12:30 +0800

    change log to en

--- yumimobileads/src/main/java/com/yumi/android/mobile/ads/utils/ZplayHandlerClickActionUtils.java
+++ yumimobileads/src/main/java/com/yumi/android/mobile/ads/utils/ZplayHandlerClickActionUtils.java
@@ -36,17 +36,16 @@ public class ZplayHandlerClickActionUtils {
         int clickType = responseAd.getAction();
 
         switch (clickType) {
-            case Constants.actiontype.ACTION_TYPE_DOWNLOAD: // 点击下载
-                ZplayDebug.v_m(TAG, "点击下载", onoff);
+            case Constants.actiontype.ACTION_TYPE_DOWNLOAD:
+                ZplayDebug.v_m(TAG, "action download", onoff);
                 handleClickDownload(responseAd, activity, type);
                 break;
-            case Constants.actiontype.ACTION_TYPE_EXPLORER: // 打开浏览器
-                ZplayDebug.v_m(TAG, "点击打开网页", onoff);
+            case Constants.actiontype.ACTION_TYPE_EXPLORER:
+                ZplayDebug.v_m(TAG, "open browser", onoff);
                 DownloadAPkBrowserBuilder.openDownloadAPKBrowser(activity, responseAd, onDismissListener);
                 break;
             case Constants.actiontype.ACTION_TYPE_OPEN_APP_STORE:
-                // 需求：判断 downloadPackageName 包名是否有值，有值打开 appStorePackage 代表的应用市场，如果该市场无法打开，则用默认应用市场打开，
-                // 均失败的话，使用使用系统浏览器打开应用
+
                 if (!TextUtils.isEmpty(responseAd.getStore_bundle()) && !TextUtils.isEmpty(responseAd.getApp_bundle())) {
                     if (openAppMarket(activity, responseAd.getStore_bundle(), responseAd.getApp_bundle())) {
                         ZplayDebug.v_m(TAG, "already opened app store", onoff);
@@ -55,18 +54,17 @@ public class ZplayHandlerClickActionUtils {
                 }
                 ZplayDebug.v_m(TAG, String.format("open app store failed, appStorePackage: %s, package: %s, delivery to default operation.", responseAd.getStore_bundle(), responseAd.getApp_bundle()), onoff);
             default:
-                ZplayDebug.v_m(TAG, "点击打开系统浏览器", onoff);
-                // 打开系统浏览器
+                ZplayDebug.v_m(TAG, "open system browser", onoff);
                 Uri uri = Uri.parse(responseAd.getTarget_url());
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 PackageManager manager = activity.getPackageManager();
                 List<ResolveInfo> activities = manager.queryIntentActivities(
                         intent, PackageManager.MATCH_DEFAULT_ONLY);
-                ZplayDebug.v_m(TAG, "总共有" + activities.size() + "个打浏览器可以打开 ", onoff);
+                ZplayDebug.v_m(TAG, "There are a total of " + activities.size() + " browsers that can be opened. ", onoff);
                 try {
                     activity.startActivity(intent);
                 } catch (Exception e) {
-                    ZplayDebug.v_m(TAG, "无法找到系统浏览器，使用内置浏览器", onoff);
+                    ZplayDebug.v_m(TAG, "not found system browser，use internal browsers", onoff);
                     DownloadAPkBrowserBuilder.openDownloadAPKBrowser(activity, responseAd, onDismissListener);
                 }
                 break;
@@ -74,19 +72,19 @@ public class ZplayHandlerClickActionUtils {
     }
 
     private static void handleClickDownload(final YumiAdBean responseBid, final Activity activity, final AdType adtype) {
-            ZplayDebug.v_m(TAG, "点击下载", onoff);
+            ZplayDebug.v_m(TAG, "click download", onoff);
             DownloadHandler.startDownload(activity, responseBid, false);
     }
 
 
 
     /**
-     * 根据投放服务传回的应用包名与url打开应用市场
+     * open App Market
      *
      * @param context        context
-     * @param marketPackages 应用市场包列表
-     * @param url            携带应用包名的链接
-     * @return 如果打开成功则返回true
+     * @param marketPackages  appStore packagename
+     * @param url            packagename
+     * @return if open AppMarket success return true
      */
     public static boolean openAppMarket(Context context, List<String> marketPackages, String url) {
         final String appPackage = getPackageNameByUrl(url);
